466e5391b1306bb7aaa834113b24aafa559cde08
hamdiahmedamin <hamdiahmedamin@users.noreply.github.com>
2023-10-29 17:53:08 +0100

    refactor

--- agriculture_management/agriculture_management/doctype/crop_cycle/test_crop_cycle.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, aminos and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestCropCycle(FrappeTestCase):
-	pass
--- agriculture_management/agriculture_management/doctype/fertilizer_content/fertilizer_content.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2023, aminos and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-
-class FertilizerContent(Document):
-	pass
